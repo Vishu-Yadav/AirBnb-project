f72c7213b055c31fb3d45e55d933b5e0318aa92b
Vishu <vishu.yadav@vlccgroup.com>
2026-01-08 16:59:42 +0530

    Added dump files and ddl files

---
+++ DDL/Resources.sql
@@ -0,0 +1,38 @@
+use database AIRBNB_PROJECT;
+
+CREATE FILE FORMAT IF NOT EXISTS csv_format
+  TYPE = 'CSV' 
+  FIELD_DELIMITER = ','
+  SKIP_HEADER = 1
+  ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
+
+show file formats;
+
+CREATE OR REPLACE STAGE snowstage
+FILE_FORMAT = csv_format
+URL='s3://test-vishu-vlcc';
+
+show stages;
+
+COPY INTO LISTINGS
+FROM @snowstage
+FILES=('listings.csv')
+CREDENTIALS=(aws_key_id = '', aws_secret_key = '');
+
+
+COPY INTO HOSTS
+FROM @snowstage
+FILES=('hosts.csv')
+CREDENTIALS=(aws_key_id = '', aws_secret_key = '');
+
+
+COPY INTO BOOKINGS
+FROM @snowstage
+FILES=('bookings.csv')
+CREDENTIALS=(aws_key_id = '', aws_secret_key = '');
+
+select * from listings;
+
+select * from hosts;
+
+select * from bookings;
