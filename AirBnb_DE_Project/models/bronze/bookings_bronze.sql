632266c593a44070077e1f4a8d17e56e927bbed9
Vishu <vishu.yadav@vlccgroup.com>
2026-01-09 16:02:15 +0530

    better method for incremental load in bronze layer

--- AirBnb_DE_Project/models/bronze/bookings_bronze.sql
+++ AirBnb_DE_Project/models/bronze/bookings_bronze.sql
@@ -1,10 +1,21 @@
-{% set incremental_load = 1 %}
-{% set incremental_col = 'CREATED_AT' %}
+{# {% set incremental_load = 1 %} #}
+{# {% set incremental_col = 'CREATED_AT' %} #}
 
 
-select * from {{ source('staging', 'bookings') }}       {# it is a jinja variable that is why we use double curly braces #}
-{% if incremental_load == 1 %}
-where {{ incremental_col }} > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})      {# 'this' keyword is used to refer to the current model #}
-{% endif %}
+{# select * from {{ source('staging', 'bookings') }}       it is a jinja variable that is why we use double curly braces #}
+{# {% if incremental_load == 1 %} #}
+{# where {{ incremental_col }} > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})      'this' keyword is used to refer to the current model #}
+{# {% endif %} #}
 
 {# SELECT MAX(CREATED_AT) FROM {{ ref("bookings_bronze") }} #}
+
+
+
+{# Above thing can be done like below #}
+{{ config(materialized = 'incremental') }}
+
+SELECT * FROM  {{ source('staging', 'bookings') }}
+
+{% if is_incremental() %}
+    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
+{% endif %}
--- AirBnb_DE_Project/models/bronze/listings_bronze.sql
+++ AirBnb_DE_Project/models/bronze/listings_bronze.sql
@@ -1,7 +1,19 @@
-{% set incremental_load = 1 %}
-{% set incremental_col = 'CREATED_AT' %}
+{# {% set incremental_load = 1 %} #}
+{# {% set incremental_col = 'CREATED_AT' %} #}
 
-select * from {{ source('staging', 'listings') }}       {# it is a jinja variable that is why we use double curly braces #}
-{% if incremental_load == 1 %}
-where {{ incremental_col }} > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})      {# 'this' keyword is used to refer to the current model #}
+{# select * from {{ source('staging', 'listings') }}       it is a jinja variable that is why we use double curly braces #}
+{# {% if incremental_load == 1 %} #}
+{# where {{ incremental_col }} > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})      'this' keyword is used to refer to the current model #}
+{# {% endif %} #}
+
+
+
+
+{# Above thing can be done like below #}
+{{ config(materialized = 'incremental') }}
+
+SELECT * FROM  {{ source('staging', 'listings') }}
+
+{% if is_incremental() %}
+    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
 {% endif %}
