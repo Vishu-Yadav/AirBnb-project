f78f3531e7c4fbcc0e643183e578d5136147d076
Vishu <vishu.yadav@vlccgroup.com>
2026-01-09 13:41:01 +0530

    jinja variable example demo.sql

--- AirBnb_DE_Project/models/demo/demo.sql
+++ AirBnb_DE_Project/models/demo/demo.sql
@@ -1,3 +1,17 @@
 {{ config(materialized='view')}}
 
 select * from AIRBNB_PROJECT.STAGING_LAYER.listings
+
+
+
+{# jinja example #}
+
+{# 1) Without Jinja #}
+select * from {{ref('bookings_bronze')}}
+where NIGHTS_BOOKED > 3
+
+{# 2) With Jinja #}
+{% set nights_booked = 3 %}                  {# this is a jinja variable which has value as 3#}
+
+select * from {{ref('bookings_bronze')}}
+where NIGHTS_BOOKED > {{ nights_booked }}
