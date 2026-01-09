dc5fed04edaaa44c9faabb1d7ec1eb6e8f1a4d75
Vishu <vishu.yadav@vlccgroup.com>
2026-01-09 14:05:49 +0530

    more jinja examples

--- AirBnb_DE_Project/models/demo/demo.sql
+++ AirBnb_DE_Project/models/demo/demo.sql
@@ -6,12 +6,38 @@ select * from AIRBNB_PROJECT.STAGING_LAYER.listings
 
 {# jinja example #}
 
-{# 1) Without Jinja #}
-select * from {{ref('bookings_bronze')}}
+{# 1) Without Jinja variable #}
+select * from {{ref('bookings_bronze')}}            {# here ref is used for Model referencing #}
 where NIGHTS_BOOKED > 3
 
-{# 2) With Jinja #}
+{# 2) With Jinja variable #}
 {% set nights_booked = 3 %}                  {# this is a jinja variable which has value as 3#}
 
 select * from {{ref('bookings_bronze')}}
 where NIGHTS_BOOKED > {{ nights_booked }}
+
+
+
+{# 3) Jinja if-else example #}
+
+{% set flag = 1 %}
+
+select * from {{ref('bookings_bronze')}}
+{% if flag == 1 %}
+    where NIGHTS_BOOKED > 3
+{% else %}
+    where NIGHTS_BOOKED <= 3
+{% endif %}
+
+
+
+{# 4) Loop #}
+
+{% set cols = ['NIGHTS_BOOKED', 'BOOKING_ID', 'BOOKING_AMOUNT'] %}
+
+SELECT 
+{% for col in cols %}
+    {{ col }}
+        {% if not loop.last %}, {% endif %}
+{% endfor %}
+FROM {{ ref('bookings_bronze') }}
